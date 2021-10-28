947001d8e89ad166693e70f4f77a3161fb56dba0
Julio <jdelacruz94@msn.com>
2021-10-28 15:04:28 +0200

    Added public folder

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ app = express();
 
 const gap = require('./routes/gap')
 
+app.use(express.static(__dirname + '/public'));
+
 app.use(cors())
 app.use(express.json())
 
 app.use('/gap', gap)
 
+app.use((req, res, next) => {
+    res.status(404)
+        .type('text')
+        .send('Not Found');
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
